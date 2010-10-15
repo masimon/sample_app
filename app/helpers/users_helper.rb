5d2409f348a0caa231da6f24e73d58158cd0e6a5
Michael Simon <masimon.0@gmail.com>
2010-10-15 13:34:10 -0400

    Workarounds until gravatar and factory-girl gems can be installed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def gravatar_for(user, options = { :size => 50 })
-    user.email
+    image_tag("pass.png", :alt => "Placeholder Avatar", :class => "gravatar")
     #gravatar_image_tag(user.email.downcase, :alt => user.name,
     #                                        :class => 'gravatar',
     #                                        :gravatar => options)
