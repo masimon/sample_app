d10636c198d5e0ab8b034c47663585c07a965062
Michael Simon <masimon.0@gmail.com>
2010-10-15 14:30:13 -0400

    User signup cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "Sign up - Error... Try Again!"
+      @user.password = ""
+      @user.password_confirmation = ""
       render 'new'
     end
   end
